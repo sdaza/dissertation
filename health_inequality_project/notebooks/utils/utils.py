54765a8a7e5972e2444a053da94978e2e5996d83
Sebastian <sebastian.daza@gmail.com>
2018-01-18 08:18:16 -0600

    update notebooks

--- health_inequality_project/notebooks/utils/utils.py
+++ health_inequality_project/notebooks/utils/utils.py
@@ -7,7 +7,29 @@ def missing_data(df):
     else:
         print('No missing data!')
         
-        
+# impute values
+def impute_values(data, group_vars):  
+    df = data.copy()
+    group= df.groupby(group_vars)
+    
+    def impute_median(series):
+        return series.fillna(series.median())
+    
+    def impute_mode(series):
+        return series.fillna(series.mode()[0])
+
+    # get list of variables with missing values
+    m = df.isnull().sum()/len(df)
+    variables = list(m[m>0.00].index)
+    
+    # impute 
+    for v in variables:
+        if (df[v].dtypes == np.dtype('int')) | (df[v].dtypes == np.dtype('float')):
+            df[v] = group[v].transform(impute_median)
+        elif (df[v].dtypes == np.dtype('object')):
+            df[v] = group[v].transform(impute_mode) # most frequent category
+    return df
+
 # transform variables
 import numpy as np 
 
@@ -21,3 +43,4 @@ def transform_variables(data, variables, kind='log'):
             values = (d[name] - np.mean(d[name])) / np.std(d[name]) # z-score
         d[kind+'_'+name] = values
     return d
+
