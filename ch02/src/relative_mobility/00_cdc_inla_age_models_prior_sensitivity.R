3fb73fa7b8642007185b3c9b5278eae8bb3decd1
Sebastian Daza <sebastian.daza@gmail.com>
2019-06-25 22:19:21 -0400

    update code

--- ch02/src/relative_mobility/00_cdc_inla_age_models_prior_sensitivity.R
+++ ch02/src/relative_mobility/00_cdc_inla_age_models_prior_sensitivity.R
@@ -1,41 +1,45 @@
+
+########################################
 # CDC mortality - income mobility
+# prior sensitivy analysis
 # author: Sebastina Daza
+########################################
+
 
 # R < 00dissertation/ch02/src/00_cdc_inla_age_models_prior_sensitivity.R > 00dissertation/ch02/src/00_cdc_inla_age_models_age_sensitivity.log  --no-save  &
 
 # load libraries
-library(here)
 library(sdazar)
 library(INLA)
 library(brinla)
 library(ggplot2)
 library(patchwork)
 library(fmsb) # life tables
+# install.packages('USAboundariesData')
 library(USAboundaries)
 library(maptools)
 library(spdep)
 library(sp)
-library(fmsb)
 library(texreg)
-# library(sf)
 
-# load functions
-setwd('00dissertation/ch02')
-source('src/utils/functions.R')
-county.adj = 'data/counties.graph' # for spatial models
+# utils
+source("src/utils/utils.R")
 
 # plot options
-options(repr.plot.width = 5, repr.plot.height = 4) # plot options
+options(repr.plot.width = 5, repr.plot.height = 4)
 
 # INLA node  options
 (nodes = parallel::detectCores())
 INLA:::inla.dynload.workaround()
-inla.setOption("num.threads", (nodes/2) - 6)
+inla.setOption("num.threads", nodes)
+
+# define county graph object for spatial models
+county.adj = 'data/counties.graph'
 
 # read data
-data = readRDS('data/cdc_chetty.rds')
-# fwrite(data, 'data/cdc_chetty.csv')
+data = readRDS('output/cdc_chetty.rds')
 
+# explore missing records
 dim(data)
 countmis(data)
 
@@ -67,40 +71,37 @@ data[, state_i := .GRP, by = state]
 
 length(unique(data[, county_i]))
 
-############################
 # aggregate data by gender
-############################
-
 male = data[sex==1]
 female = data[sex==2]
 
 amale = male[,
-            .(state = first(state),
-              deaths = sum(deaths),
-              deaths1 = sum(deaths1),
-              deaths2 = sum(deaths2),
-              deaths3 = sum(deaths3),
-              deaths4 = sum(deaths4),
-              pop = sum(pop)),
-            by = .(age, county)]
+             .(state = first(state),
+               deaths = sum(deaths),
+               deaths1 = sum(deaths1),
+               deaths2 = sum(deaths2),
+               deaths3 = sum(deaths3),
+               deaths4 = sum(deaths4),
+               pop = sum(pop)),
+             by = .(age, county)]
 
 afemale = female[,
-                .(state = first(state),
-                  deaths = sum(deaths),
-                  deaths1 = sum(deaths1),
-                  deaths2 = sum(deaths2),
-                  deaths3 = sum(deaths3),
-                  deaths4 = sum(deaths4),
-                  pop = sum(pop)),
-                by = .(age, county)]
+                 .(state = first(state),
+                   deaths = sum(deaths),
+                   deaths1 = sum(deaths1),
+                   deaths2 = sum(deaths2),
+                   deaths3 = sum(deaths3),
+                   deaths4 = sum(deaths4),
+                   pop = sum(pop)),
+                  by = .(age, county)]
 
 male_cov = male[, s := 1:.N, by = .(age, county)][s==1,][
-    , c('pop','deaths', 'deaths1', 'deaths2', 'deaths3',
-        'deaths4', 'state', 'race', 's', 'sex') := NULL]
+                , c('pop','deaths', 'deaths1', 'deaths2', 'deaths3',
+                    'deaths4', 'state', 'race', 's', 'sex') := NULL]
 
 female_cov = female[, s := 1:.N, by = .(age, county)][s==1,][
-    , c('pop','deaths', 'deaths1', 'deaths2', 'deaths3',
-        'deaths4', 'state', 'race', 's', 'sex') := NULL]
+                    , c('pop','deaths', 'deaths1', 'deaths2', 'deaths3',
+                        'deaths4', 'state', 'race', 's', 'sex') := NULL]
 
 men = merge(amale, male_cov, by = c('age', 'county'))
 women = merge(afemale, female_cov, by = c('age', 'county'))
@@ -131,122 +132,122 @@ dim(women)
 formula = deaths ~  1 + log_population + log_income + z_relative_mob +
           z_gini + z_segregation_income + log_unemployment + log_pct_hispanic +
           log_pct_black + z_uninsured + z_medicare_expenses +
-        f(state_i, model = 'iid') +
-        f(county_i, model = 'iid') +
-        f(age, model = 'iid') +
-        f(id, model = 'iid') +
-        f(mob_age, z_relative_mob, model = 'iid') +
-        f(gini_age, z_gini, model = 'iid')
+          f(state_i, model = 'iid') +
+          f(county_i, model = 'iid') +
+          f(age, model = 'iid') +
+          f(id, model = 'iid') +
+          f(mob_age, z_relative_mob, model = 'iid') +
+          f(gini_age, z_gini, model = 'iid')
 
 m1 = inla(formula, data = men,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 w1 = inla(formula, data = women,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 # m2 (pc(1, .10)
 
 pcprior = list(prec = list(prior="pc.prec",
-  param = c(1, 0.10)))
+                           param = c(1, 0.10)))
 
 formula = deaths ~  1 + log_population + log_income + z_relative_mob +
           z_gini + z_segregation_income + log_unemployment + log_pct_hispanic +
           log_pct_black + z_uninsured + z_medicare_expenses +
-        f(state_i, model = 'iid', hyper=pcprior) +
-        f(county_i, model = 'iid', hyper=pcprior) +
-        f(age, model = 'iid', hyper=pcprior) +
-        f(id, model = 'iid', hyper=pcprior) +
-        f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
-        f(gini_age, z_gini, model = 'iid', hyper=pcprior)
+          f(state_i, model = 'iid', hyper=pcprior) +
+          f(county_i, model = 'iid', hyper=pcprior) +
+          f(age, model = 'iid', hyper=pcprior) +
+          f(id, model = 'iid', hyper=pcprior) +
+          f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
+          f(gini_age, z_gini, model = 'iid', hyper=pcprior)
 
 m2 = inla(formula, data = men,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 w2 = inla(formula, data = women,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 # m3 (pc(10, .10))
 
 pcprior = list(prec = list(prior="pc.prec",
-  param = c(10, 0.10)))
+               param = c(10, 0.10)))
 
 
 formula = deaths ~  1 + log_population + log_income + z_relative_mob +
           z_gini + z_segregation_income + log_unemployment + log_pct_hispanic +
           log_pct_black + z_uninsured + z_medicare_expenses +
-        f(state_i, model = 'iid', hyper=pcprior) +
-        f(county_i, model = 'iid', hyper=pcprior) +
-        f(age, model = 'iid', hyper=pcprior) +
-        f(id, model = 'iid', hyper=pcprior) +
-        f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
-        f(gini_age, z_gini, model = 'iid', hyper=pcprior)
+          f(state_i, model = 'iid', hyper=pcprior) +
+          f(county_i, model = 'iid', hyper=pcprior) +
+          f(age, model = 'iid', hyper=pcprior) +
+          f(id, model = 'iid', hyper=pcprior) +
+          f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
+          f(gini_age, z_gini, model = 'iid', hyper=pcprior)
 
 m3 = inla(formula, data = men,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 w3 = inla(formula, data = women,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 # m4 (pc(10, 0.01))
 
 pcprior = list(prec = list(prior="pc.prec",
-  param = c(10, 0.01)))
+                           param = c(10, 0.01)))
 
 formula = deaths ~  1 + log_population + log_income + z_relative_mob +
           z_gini + z_segregation_income + log_unemployment + log_pct_hispanic +
           log_pct_black + z_uninsured + z_medicare_expenses +
-        f(state_i, model = 'iid', hyper=pcprior) +
-        f(county_i, model = 'iid', hyper=pcprior) +
-        f(age, model = 'iid', hyper=pcprior) +
-        f(id, model = 'iid', hyper=pcprior) +
-        f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
-        f(gini_age, z_gini, model = 'iid', hyper=pcprior)
+          f(state_i, model = 'iid', hyper=pcprior) +
+          f(county_i, model = 'iid', hyper=pcprior) +
+          f(age, model = 'iid', hyper=pcprior) +
+          f(id, model = 'iid', hyper=pcprior) +
+          f(mob_age, z_relative_mob, model = 'iid', hyper=pcprior) +
+          f(gini_age, z_gini, model = 'iid', hyper=pcprior)
 
 m4 = inla(formula, data = men,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 w4 = inla(formula, data = women,
-                family = 'poisson', E = pop,
-                control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
-                  cpo=TRUE),
-                control.predictor = list(link = 1, compute = TRUE),
-                control.inla = list(strategy = 'gaussian')
-              )
+          family = 'poisson', E = pop,
+          control.compute = list(config=TRUE, dic=TRUE, waic=TRUE,
+                                 cpo=TRUE),
+          control.predictor = list(link = 1, compute = TRUE),
+          control.inla = list(strategy = 'gaussian')
+          )
 
 
 # create table
@@ -286,7 +287,7 @@ texreg(m_models,
             groups = list("Random Effects" = c(4:9)),
             custom.note = "Note: Selected coefficients (mean of marginal posterior distribution).
             Poisson model with offset = \\texttt{log(population)}. 95\\% credibility intervals.",
-             file = "output/m_age_prior_sensitivity.tex")
+            file = "output/m_age_prior_sensitivity.tex")
 
 
 # women
--- related_projects/health_inequality_project/readme.md
+++
@@ -1,3 +0,0 @@
-# Addressing the Longevity Gap between the Rich and Poor: The Role of Income Mobility
-
-## Directions
