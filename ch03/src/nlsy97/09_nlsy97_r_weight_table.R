11e7e849cb182e94ce150a047865240eb89d4a75
Sebastian Daza <sebastian.daza@gmail.com>
2020-06-17 15:10:05 +0200

    update code

--- ch03/src/nlsy97/09_nlsy97_r_weight_table.R
+++ ch03/src/nlsy97/09_nlsy97_r_weight_table.R
@@ -1,6 +1,6 @@
 ##############################
 # county income mobility and individual health
-# PSID IPT weights table
+# NLSY IPT weights table
 # author: sebastian daza
 ##############################
 
@@ -10,7 +10,7 @@ library(xtable)
 source("src/utils.R")
 
 # read output
-models = readRDS("output/data/psid_adjusted_models.rds")
+models = readRDS("output/data/nlsy97_adjusted_models.rds")
 n_imputations = 20
 
 # continous exposure table
@@ -26,16 +26,16 @@ for (i in seq_along(model_names)) {
     list_weights[[i]] = models[[model_names[i]]][["weights"]]
 }
 
-comment = paste0("Analyses based on exposure from 1 to 20 years old. ",
-    paste0("Statisticis based on  ", n_imputations, " multiple imputed datasets.")
+comment = paste0("Analyses based on exposure from 12 to 20 years old. ",
+    paste0("Statistics based on  ", n_imputations, " multiple imputed datasets.")
 )
 
 tableWeights(list_weights,
     model_names = c("\\quad Rank-rank", "\\quad Upward mobility", "\\quad Gini",
         "\\quad Rank-rank", "\\quad Upward mobility", "\\quad Gini"),
-    caption  = "PSID Stabilized treatment weights",
-    label = "tab:psid_ipt_weights",
+    caption  = "NLSY97 Stabilized treatment weights (residualized)",
+    label = "tab:nlsy97_ipt_weigths",
     comment = comment,
-    filename = "output/tables/psid_ipt_weights.tex",
+    filename = "output/tables/nlsy97_r_ipt_weights.tex",
     tabcolsep = 10,
     arraystretch = 1)
