07f3bf4078f6283b7e165e46ea306aff138896be
Sebastian Daza <sebastian.daza@gmail.com>
2020-08-18 15:52:11 +0200

    update model

--- ch04/src/testing/01_testing_output.R
+++ ch04/src/testing/01_testing_output.R
@@ -6,7 +6,7 @@
 
 library(data.table)
 
-# read data
+# read family data
 f = fread("models/MobHealthRecycling/output/family.csv")
 names(f)
 
@@ -16,10 +16,29 @@ table(f$kid_age)
 prop.table(table(f[parent_generation == 0, parent_type]))
 
 # rigth distribution
-prop.table(table(f[parent_generation %in% c(1:5), parent_type]))
+prop.table(table(f[parent_generation %in% c(1:10), parent_type]))
 
 # event distribution
-prop.table(table(f[kid_age > 0, .(parent_type, kid_type)]))
-prop.table(table(f[kid_age == 0, .(parent_type, kid_type)]))
+prop.table(table(f[kid_age > 0, .(parent_type, kid_type)]), 1)
+prop.table(table(f[kid_age == 0, .(parent_type, kid_type)]), 1)
 
 hist(f[parent_generation == 2, parent_age])
+
+# read mortality data
+m = fread("models/MobHealthRecycling/output/mortality.csv")
+print(paste0("Date of simulation: ", m$date[1]))
+
+prop.table(table(m[generation == 0, smoker]))
+
+mean(m[generation == 4, age])
+
+m[generation == 4, mean(age), income_type]
+m[generation == 6, mean(age), .(smoker)]
+
+prop.table(table(m[generation == 4, .(income_type, smoker)]), 1)
+
+test = m[generation == 5, .(age = mean(age)), .(income_type, smoker)]
+setorder(test,  income_type)
+test
+
+hist(m[generation == 2, age], breaks = 10)
