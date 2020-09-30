4234fc686d7dc9cf4967f6e7706fe054e78838a2
Sebastian Daza <sebastian.daza@gmail.com>
2020-09-30 15:57:07 +0200

    update

--- ch04/src/verification/02_segregation.R
+++ ch04/src/verification/02_segregation.R
@@ -11,30 +11,32 @@ source("src/utils.R")
 path = "models/MobHealthRecycling/output/"
 
 # read files
-par = readMultipleFiles("parameters", path)
-dat = readMultipleFiles("environ", path)
+# par = readMultipleFiles("parameters", path)
+# dat = readMultipleFiles("environ", path)
 
-sel = par[, .(iteration, replicate, counties, people_per_county, move_random, move_threshold, max_generation)]
+# sel = par[, .(iteration, replicate, counties, people_per_county, move_random, move_threshold, max_generation)]
 
-dat = merge(dat, sel, by = c("iteration", "replicate"))
-temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE), min = min(nsi, na.rm = TRUE), max = max(nsi, na.rm = TRUE), 
-        avg_time = mean(time), avg_pop = mean(population)), 
-    .(iteration, move_random, move_threshold, max_generation)]
+# dat = merge(dat, sel, by = c("iteration", "replicate"))
+# temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE), min = min(nsi, na.rm = TRUE), max = max(nsi, na.rm = TRUE),
+#         avg_time = mean(time), avg_pop = mean(population)),
+#     .(iteration, move_random, move_threshold, max_generation)]
 
-saveRDS(dat, "output/data/nsi_21_25.rds")
+# saveRDS(dat, "output/data/nsi_21_25.rds")
+
+# read RDS
 dat = readRDS("output/data/nsi_21_25.rds")
 
-
+# create plots
 savepdf("output/plots/nsi_21")
-ggplot(dat, aes(time, nsi, group = replicate, color = iteration)) + geom_line( alpha = 0.4, size = 0.1) + 
-    labs(x = "\
-    nYear", y = "Neighborhood sorting index (NSI)\n")  + 
+ggplot(dat[iteration == 1], aes(time, nsi, group = replicate)) + geom_line( alpha = 0.2, size = 0.1) +
+    labs(x = "\nYear", y = "Neighborhood sorting index (NSI)\n")  +
+    xlim(0, 1550) + ylim(0, 1) +
     theme_minimal()
 dev.off()
 
 savepdf("output/plots/nsi_25")
-ggplot(dat[iteration == 2], aes(time, nsi, group = replicate)) + geom_line( alpha = 0.4, size = 0.1) + 
-    labs(x = "\nYear", y = "Neighborhood sorting index (NSI)\n")  + 
+ggplot(dat[iteration == 2], aes(time, nsi, group = replicate)) + geom_line(alpha = 0.2, size = 0.1) +
+    labs(x = "\nYear", y = "Neighborhood sorting index (NSI)\n")  +
+    xlim(0, 1550) + ylim(0, 1) +
     theme_minimal()
 dev.off()
-
