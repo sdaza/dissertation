0e3f44db78ad6d8a02531cac384270b72d528cbb
Sebastian Daza <sebastian.daza@gmail.com>
2020-09-30 13:24:47 +0200

    update

--- ch04/src/utils.R
+++ ch04/src/utils.R
@@ -1,4 +1,9 @@
+#################################
+# generative model of income mobility and mortality
 # auxiliary functions
+# author: sebastian daza
+################################
+
 
 readMultipleFiles = function(pattern, path) {
     files = list.files(path, pattern)
@@ -7,3 +12,12 @@ readMultipleFiles = function(pattern, path) {
     m =  rbindlist(l)
     return(m)
 }
+
+
+savepdf <- function(file, width = 16, height = 10) {
+  fname <- paste0(file, ".pdf")
+  pdf(fname, width = width / 2.54, height = height / 2.54,
+      pointsize = 10)
+  par(mgp = c(2.2, 0.45, 0), tcl = -0.4, mar = c(3.3, 3.6, 1.1, 1.1))
+}
+
