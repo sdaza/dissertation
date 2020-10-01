95f5694c6b83b2d546ed78aa148a374b70897fef
Sebastian Daza <sebastian.daza@gmail.com>
2020-10-01 10:38:33 +0200

    update population dynamics plots

--- ch04/src/verification/01_population.R
+++ ch04/src/verification/01_population.R
@@ -11,29 +11,56 @@ source("src/utils.R")
 path = "models/MobHealthRecycling/output/"
 
 # read files
-# par = readMultipleFiles("parameters", path)
-# dat = readMultipleFiles("environ", path)
+par = readMultipleFiles("parameters", path)
+dat = readMultipleFiles("environ", path)
 
-# sel = par[, .(iteration, replicate, counties, people_per_county, move_random, move_threshold, max_generation)]
+sel = par[, .(iteration, replicate, counties, people_per_county, move_random, 
+    move_threshold, max_generation)]
+dat = merge(dat, sel, by = c("iteration", "replicate"))
+d RDS
+saveRDS(dat, "output/data/population.rds")
+dat = readRDS("output/data/population.rds")
 
-# dat = merge(dat, sel, by = c("iteration", "replicate"))
-# temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE), min = min(nsi, na.rm = TRUE), max = max(nsi, na.rm = TRUE),
-#         avg_time = mean(time), avg_pop = mean(population)),
-#     .(iteration, move_random, move_threshold, max_generation)]
-
-# saveRDS(dat, "output/data/nsi_21_25.rds")
-
-# read RDS
-dat = readRDS("output/data/nsi_21_25.rds")
-
+# population
 savepdf("output/plots/population")
-ggplot(dat[iteration == 2], aes(time, population, group = replicate)) + geom_line( alpha = 0.2, size = 0.1) +
+ggplot(dat, aes(time, population, group = replicate)) + geom_line( alpha = 0.2, size = 0.1) +
     labs(x = "\nYear", y = "Population\n")  +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 8)) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 8)) + 
     theme_minimal()
 dev.off()
 
+# age of death distribution
+m = readMultipleFiles("mort", path)
+saveRDS(m, "output/data/mortality.rds")
+m = readRDS("output/data/mortality.rds")
+
+savepdf("output/plots/age_death")
+ggplot(m, aes(x = age)) + geom_histogram(binwidth = 1.2, color = "black", fill="white") + 
+    labs(x = "\nAge of death", y = "Frequency\n") + 
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 8)) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 8)) +
+theme_minimal() 
+dev.off()
+
+# average number of kids 
+savepdf("output/plots/num_kids")
+ggplot(m, aes(x = nkids)) + geom_histogram(binwidth = 1, color = "black", fill="white") + 
+    labs(x = "\nNumber of kids", y = "Frequency\n") + 
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 8)) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 10)) +
+    geom_vline(aes(xintercept=mean(nkids)),
+            color="gray", linetype="dashed", size = 0.4) + 
+theme_minimal() 
+dev.off()
+
+# life expectancy
 savepdf("output/plots/le")
-ggplot(dat[iteration == 2], aes(time, le, group = replicate)) + geom_line( alpha = 0.2, size = 0.1) +
-    labs(x = "\nYear", y = "Life expectancy\n")  +
-    theme_minimal()
+ggplot(unique(dat[le > 0, .(le)]), aes(x = le)) + geom_histogram(color = "black", fill="white") + 
+    labs(x = "\nLife expectancy", y = "Frequency\n") + 
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 8)) +
+    scale_x_continuous(breaks = scales::pretty_breaks(n = 6)) +
+    geom_vline(aes(xintercept=mean(le)),
+            color="gray", linetype="dashed", size = 0.4) + 
+theme_minimal() 
 dev.off()
