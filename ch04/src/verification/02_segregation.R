da9a91b7c986c24ce9fe3b2d2fea3d2fe1876adf
Sebastian Daza <sdaza@linstat14.ssc.wisc.edu>
2020-10-06 05:44:50 -0500

    update

--- ch04/src/verification/02_segregation.R
+++ ch04/src/verification/02_segregation.R
@@ -14,12 +14,11 @@ path = "models/MobHealthRecycling/output/"
 par = readMultipleFiles("parameters", path)
 dat = readMultipleFiles("environ", path)
 
-# selection of parameters
-sel = par[, .(iteration, replicate, counties, people_per_county, 
-    move_random, move_threshold, max_generation)]
+sel = par[, .(iteration, replicate, counties, people_per_county, move_random,
+    move_threshold, max_generation)]
 
 dat = merge(dat, sel, by = c("iteration", "replicate"))
-temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE), 
+temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE),
     min = min(nsi, na.rm = TRUE), max = max(nsi, na.rm = TRUE),
     avg_time = mean(time), avg_pop = mean(population)),
     .(iteration, move_random, move_threshold, max_generation)]
@@ -28,17 +27,24 @@ temp = dat[, .(.N, avg_nsi = mean(nsi, na.rm = TRUE),
 saveRDS(dat, "output/data/segregation.rds")
 dat = readRDS("output/data/segregation.rds")
 
-unique(par[, names(par) != "replicate", with=FALSE])
+tab = dat[!is.na(nsi), .(nsi = mean(nsi), threshold = max(move_threshold),
+    sd = sd(nsi)), iteration]
+tab[, prop := sd / nsi]
+tab
+
+# parameters
+unique(dat[, .(iteration, move_threshold)])
 
 # create plots
-plot_names = c("random", "21", "23", "24", "25","28", "35", "40", "45")
+plot_names = c("random", as.character(c(21, 23:25, 28, 35, 40, 45)))
+
 for (i in seq_along(plot_names)) {
     savepdf(paste0("output/plots/nsi_", plot_names[i]))
     print(
-    ggplot(dat[!is.na(nsi) & iteration == i], aes(time, nsi, group = replicate)) + geom_line( alpha = 0.20, size = 0.1) +
+    ggplot(dat[!is.na(nsi) & iteration == i], aes(time, nsi, group = replicate)) + geom_line( alpha = 0.25, size = 0.1) +
         labs(x = "\nYear", y = "NSI\n")  +
         scale_y_continuous(limits = c(0, 1), breaks = scales::pretty_breaks(n = 6)) +
-        scale_x_continuous(limits = c(0, 1550), breaks = scales::pretty_breaks(n = 8)) + 
+        scale_x_continuous(limits = c(0, 1550), breaks = scales::pretty_breaks(n = 8)) +
         theme_minimal()
     )
     dev.off()
