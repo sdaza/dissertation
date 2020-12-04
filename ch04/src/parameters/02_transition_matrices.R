34c91a95b4eb0ac87fc971365dee281e27e79ba6
Sebastian Daza <sebastian.daza@gmail.com>
2020-12-04 14:23:48 +0100

    update model

--- ch04/src/parameters/02_transition_matrices.R
+++ ch04/src/parameters/02_transition_matrices.R
@@ -8,11 +8,40 @@
 library(data.table)
 library(xlsx)
 
+table = function (...) base::table(..., useNA = 'ifany')
+cor = function(...) stats::cor(..., use = "complete.obs")
+
+correct_decimals = function(x) {
+    as.numeric(gsub(",", "\\.", x))
+}
+
+# baseline transition matrix
+values = c(0.337,	0.280,	0.184,	0.123,	0.075,
+    0.242,	0.242,	0.217,	0.176,	0.123,
+    0.178,	0.198,	0.221,	0.220,	0.183,
+    0.134,	0.160,	0.209,	0.244,	0.254,
+    0.109,	0.119, 0.170,	0.236,	0.365)
+
+t = matrix(values, 5, 5, byrow =  TRUE)
+
+diag(t) = 0
+
+v = apply(t, 1, sum)
+for (i in seq_along(v)) {
+ t[i,] = t[i, ] /  v[i]
+}
+
+apply(t, 1, sum)
+diag(t) = 1.0
+
 # read data and transform transition matrices
 covs = fread("data/cz_covs.csv")
+im = fread("data/cz_income_mobility.csv")
+im = im[, lapply(.SD, correct_decimals)]
+im = im[complete.cases(im)]
 
 # transition matrices
-t = fread("data/transition_matrix_income.csv")
+t = fread("data/cz_transition_matrix.csv")
 n = names(t)
 n = gsub("^P\\(Child |Par |\\)", "", n)
 n = gsub(" \\|", "_", n)
@@ -28,35 +57,54 @@ tt = melt(t, id.vars = c("cz", "children"),
     variable.name = "parent",
     value.name = c("q1", "q2", "q3", "q4", "q5"))
 tt = tt[complete.cases(tt)]
-write.xlsx(tt, "data/transition_matrices_cz.xlsx", row.names = FALSE)
+
+tt[cz == 21302]
+
+write.xlsx(tt, "data/cz_transition_matrices.xlsx", row.names = FALSE)
 
 # merge with covs
 setnames(covs, names(covs), tolower(names(covs)))
 covs[, pop := as.numeric(gsub("\\.", "", pop))]
 vars = c("black", "racial_seg", "income_seg", "seg_pov", "seg_affluence",
     "prop_comute_15", "hhincome", "gini", "crime", "income_growth")
-covs[, (vars) := lapply(.SD, function(x) as.numeric(gsub(",", "\\.", x))), .SDcols = vars]
+covs[, (vars) := lapply(.SD, correct_decimals), .SDcols = vars]
 
 dim(tt)
 tt = merge(tt, covs, by = "cz", all.x = TRUE)
+tt = merge(tt, im, by = "cz", all.x = TRUE)
 dim(tt)
 
 tt[, lincome := log(hhincome)]
 
-tp = melt(tt, id.vars = c("cz", "pop", "parent", "black", "income_seg", "seg_pov", "lincome"),
+tp = melt(tt, id.vars = c("cz", "pop", "rm", "am", "parent", "black", "income_seg", "seg_pov", "lincome"),
     measure = patterns("^q[1-5]"), value.name = "prop", variable.name = "child")
 tp[, child := as.numeric(gsub("q", "", child))]
-write.xlsx(tp, "data/long_transition_matrices_cz.xlsx", row.names = FALSE)
+write.xlsx(tp, "data/cz_long_format_transition_matrices.xlsx", row.names = FALSE)
+
+# testing
+sum(tp[cz == 30100 & parent ==1, .(cz, parent, child, prop)]$prop)
 
 # exploring data
 hist(tp[parent == 5 & parent == child, prop])
 hist(tp[parent == 1 & parent == child, prop])
 
-s = tp[, quantile(prop, c(0.95)), .(parent, child)]
-setorder(s, parent)
-s
+tp[parent == ]
 
-cor(tp[parent == child, .(lincome, income_seg, black, prop)])
+props = seq(0.1, 0.9, 1/10)
+datasets = list()
+for (i in seq_along(props)) {
+    datasets[[i]] = tp[, .(prop = quantile(prop, props[i])), .(parent, child)][, percentile := props[i] * 100]
+}
+stp = rbindlist(datasets)
+stp
+
+stp[parent == 1]
+
+setorder(s, parent)
+
+
+
+cor(tp[parent == child, .(lincome, income_seg, black, prop, rm)])
 tp[parent == child, .(mean(prop)), parent]
 plot(tp[parent == child, .(log_prop_black = log(black),
     log_prop = log(prop))])
@@ -67,14 +115,23 @@ plot(tp[parent %in% c(1,5) & parent == child, .(log_prop_black = log(black),
 plot(tp[parent %in% c(1,5) & parent == child, .(log_income = lincome,
     log_prop = log(prop))])
 
-cor(tp[parent == 1 & parent == child, .(lincome, prop)])
-cor(tp[parent == 5 & parent == child, .(lincome, prop)])
+cor(tp[parent == 1 & parent == child, .(prop, rm)])
+cor(tp[parent == 2 & parent == child, .(prop, rm)])
+cor(tp[parent == 3 & parent == child, .(prop, rm)])
+cor(tp[parent == 4 & parent == child, .(prop, rm)])
+cor(tp[parent == 5 & parent == child, .(prop, rm)])
 
-plot(tp[parent == 1 & parent == child, .(lincome, prop)])
-plot(tp[parent == 5 & parent == child, .(lincome, prop)])
+cor(tp[parent == 1 & parent == child, .(prop, black)])
+cor(tp[parent == 1 & parent == child, .(rm, black)])
+
+plot(tp[parent == 1 & parent == child, .(prop, rm)])
+plot(tp[parent == 2 & parent == child, .(prop, rm)])
+plot(tp[parent == 5 & parent == child, .(prop, rm)])
 
 cor(tt[parent == 5, .(black,  q5)])
 cor(tt[parent == 1, .(black, q1)])
 
 summary(tt[parent == 1, q1])
 summary(tt[parent == 5, q5])
+
+tp[cz == 16802]
--- ch04/src/testing/07_income_cohort_verification.R
+++ ch04/src/testing/07_income_cohort_verification.R
@@ -8,15 +8,8 @@
 library(data.table)
 path = "models/MobHealthRecycling/output/verification/testing/"
 income = fread(paste0(path, "income_generation.csv"))
-m = fread(paste0(path, "mortality.csv"))
-cty = fread(paste0(path, "county.csv"))
 
-dim(cty)
-table(cty$model_time)
-dim(m)
-
-table(m$generation)
-
+# functions
 perc.rank = function(x) trunc(rank(x))/length(x)
 reg = function(kid_income, parent_income, relative = TRUE) {
     m = lm(kid_income ~ parent_income)
@@ -26,6 +19,21 @@ reg = function(kid_income, parent_income, relative = TRUE) {
         return (c[1]  + 0.25 * c[2])
     }
 }
+
+addDiagonalProbs = function(parent_type, kid_type, index = 1) {
+    diag(prop.table(table(parent_type, kid_type), 1))[index]
+}
+
+# read data
+m = fread(paste0(path, "mortality.csv"))
+cty = fread(paste0(path, "county.csv"))
+
+dim(cty)
+table(cty$model_time)
+dim(m)
+
+table(m$generation)
+
 
 anyDuplicated(income$kid_id)
 table(income$cohort)
@@ -48,7 +56,7 @@ c10[, rank_kid_c := perc.rank(kid_income), county]
 names(cty)
 a = c10[, .(rank_slope_a = reg(rank_kid, rank_parent),
     rank_absolute_mob_a = reg(rank_kid, rank_parent, relative = FALSE),
-    rank_correlation_a = cor(rank_parent_c, rank_kid_c), 
+    rank_correlation_a = cor(rank_parent_c, rank_kid_c),
     kid_income_a = mean(kid_income), parent_income_a = mean(parent_income)), county]
 setorder(a, county)
 
@@ -105,6 +113,33 @@ ct[, lincome := logIncome(mean_income)]
 c1 = lmer(le ~ rank_slope + gini + lincome + as.factor(model_time)  + (1|county), data = ct)
 screenreg(list(c1))
 
+# income, transition matrix relationship
+table(income$cohort)
+prop.table(table(income[, .(parent_type, kid_type)]), 1)
+test = income[cohort > 200]
+
+test[, rank_parent := perc.rank(parent_income)]
+test[, rank_kid := perc.rank(kid_income)]
+test[, rm := reg(rank_kid, rank_parent), county]
+
+test[, p1 := addDiagonalProbs(parent_type, kid_type), county]
+test[, p2 := addDiagonalProbs(parent_type, kid_type, 2), county]
+test[, p3 := addDiagonalProbs(parent_type, kid_type, 3), county]
+test[, p4 := addDiagonalProbs(parent_type, kid_type, 4), county]
+test[, p5 := addDiagonalProbs(parent_type, kid_type, 5), county]
+test[, N := 1:.N, county]
+ct = test[N == 1]
+dim(ct)
+hist(ct$rm)
+
+cor(ct[, .(p1, rm)])
+cor(ct[, .(p2, rm)])
+cor(ct[, .(p3, rm)])
+cor(ct[, .(p4, rm)])
+cor(ct[, .(p5, rm)])
+
+plot(ct[, .(p1, rm)])
+plot(ct[, .(p5, rm)])
 
 
 
